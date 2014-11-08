e796e80b37410c79033c81ee3a7f67a3af242d92
Aaron Schumacher <ajschumacher@gmail.com>
2014-11-08 13:48:29 -0500

    working script and 727 resources in files

--- get_pages.py
+++ get_pages.py
@@ -2,7 +2,11 @@
 
 import requests
 from lxml import html
+import re
+import os
 import codecs
+
+URL = 'http://www.cjccresourcelocator.net/ResourceLocator/List.aspx'
 
 
 def helper_fields(html_text):
@@ -13,11 +17,40 @@ def helper_fields(html_text):
     result['__EVENTARGUMENT'] = ''
     return result
 
-URL = 'http://www.cjccresourcelocator.net/ResourceLocator/List.aspx'
-base = requests.get(URL)
-params = helper_fields(base.text)
-params['__EVENTTARGET'] = 'ctl00$ContentPlaceHolder1$lnk1'
 
-x = requests.post(URL, params)
-with codecs.open('test.html', 'w', 'utf-8') as f:
-    f.write(x.text)
+def targets_from(html_text):
+    return re.findall("doPostBack\('(.+?)'", html_text)
+
+
+def get_page(target, context):
+    context.update({'__EVENTTARGET': target})
+    result = requests.post(URL, context)
+    return result.text
+
+
+def write_page(html_text, filename):
+    path = os.path.join('html', filename)
+    with codecs.open(path, 'w', 'utf-8') as f:
+        f.write(html_text)
+
+
+def main():
+
+    base = requests.get(URL)
+    base_params = helper_fields(base.text)
+    base_targets = targets_from(base.text)
+
+    for list_index, base_target in enumerate(base_targets):
+        print list_index, base_target
+        list_page = get_page(base_target, base_params)
+        list_params = helper_fields(list_page)
+        list_targets = targets_from(list_page)[35:]
+        for item_index, list_target in enumerate(list_targets):
+            print item_index, list_target
+            result = get_page(list_target, list_params)
+            filename_structure = "page{0:03d}item{1:03d}.html"
+            filename = filename_structure.format(list_index, item_index)
+            write_page(result, filename)
+
+if __name__ == '__main__':
+    main()
